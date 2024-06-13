e000737b15b1f12ea920b2849afcef969b60449a
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-13 21:11:54 +0300

    Change ItemEdit: add edit functional  #7

--- src/components/common/ItemEdit/ItemEdit.jsx
+++ src/components/common/ItemEdit/ItemEdit.jsx
@@ -1,21 +1,63 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
+
+import { StoreContext } from "@/contexts/StoreProvider";
 
 import s from "./ItemEdit.module.scss";
 
 const ItemEdit = ({item, onBlur}) => {
+    const {storeData, setStoreData} = useContext(StoreContext);
+    const [inputValue, setInputValue] = useState(item.value);
+
     const inputRef = useRef(null);
 
-    useEffect(() => inputRef.current.focus())
+    const handlerChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const handlerDelete = () => {
+        const changedTodos = storeData.todos.filter((todo) => todo.id !== item.id);
+
+        setStoreData(prev => ({...prev, todos: [...changedTodos]}));
+    };
+
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+
+        if (inputValue.trim() === "") {
+            handlerDelete();
+            return;
+        }
+
+        const changedTodos = storeData.todos
+            .map((todo) => todo.id === item.id ? {...todo, value: inputValue} : todo);
+
+        setStoreData(prev => ({...prev, todos: [...changedTodos]}));
+
+        inputRef.current.blur();
+    }
+
+    const handlerKeyDown = (event) => {
+        if (event.key !== "Escape") return;
+
+        inputRef.current.blur();
+    }
+
+    useEffect(() => inputRef.current.focus());
 
     return(
-        <div className={s.itemEdit}>
+        <form 
+            className={s.itemEdit}
+            action="#" 
+            method="post" 
+            onSubmit={handlerSubmit}>
             <input 
                 type="text"
-                value={item.value}
+                value={inputValue}
                 ref={inputRef}
-                onChange={() => {}}
-                onBlur={onBlur}/>
-        </div>
+                onChange={handlerChange}
+                onBlur={onBlur}
+                onKeyDown={handlerKeyDown}/>
+        </form>
     );
 }
 
