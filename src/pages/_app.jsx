435569f05ee3715f67aa46dd5b8cc926e8f1969e
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-18 18:44:23 +0300

    change store: add localstorage functional #18

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux";
-import { store } from "@/store/store";
+import store from "@/store/store";
 
 import "@/styles/globals.scss";
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,33 @@
 import { createStore } from "redux";
 import rootReducer from "@/reducers/reducer";
 
-export const store = createStore(rootReducer);
+
+const saveToLocalStorage = (state) => {
+    try {
+        const serialisedState = JSON.stringify(state);
+
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (error) {
+        console.warn(error);
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+
+        if (serialisedState === null) return undefined;
+
+        return JSON.parse(serialisedState);
+    }   catch (e) {
+        console.warn(e);
+
+        return undefined;
+    }
+}
+
+const store = createStore(rootReducer, loadFromLocalStorage());
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
