9c1de8a02dd960971f06c9cdc807d7b731970dab
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-19 21:08:15 +0300

    change store: separate local-storage #2

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,7 @@
 import { createStore } from "redux";
+
 import rootReducer from "@/reducers/reducer";
-
-const saveToLocalStorage = (state) => {
-    try {
-        const serialisedState = JSON.stringify(state);
-
-        localStorage.setItem("persistantState", serialisedState);
-    } catch (error) {
-        console.warn(error);
-    }
-};
-
-const loadFromLocalStorage = () => {
-    try {
-        const serialisedState = localStorage.getItem("persistantState");
-
-        if (serialisedState === null) return undefined;
-
-        return JSON.parse(serialisedState);
-    } catch (e) {
-        console.warn(e);
-
-        return undefined;
-    }
-};
+import { loadFromLocalStorage, saveToLocalStorage } from "./localStorage";
 
 const store = createStore(rootReducer, loadFromLocalStorage());
 
