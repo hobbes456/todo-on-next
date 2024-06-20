6631e9cc830ac4391122ba1b0ba71bee1f146853
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-20 21:05:13 +0300

    delete actions.js #6

--- src/constants/actions.js
+++
@@ -1,7 +0,0 @@
-export const TODO_ADD = "todos/todoAdded";
-export const TODO_DELETED = "todos/todoDeleted";
-export const TODO_EDITED = "todos/todoEdited";
-export const TODO_TOGGLED = "todos/todoToggled";
-export const TODO_ALL_COMPLETED = "todos/todoAllCompleted";
-export const TODO_CLEAR_COMPLETED = "todos/todoClearCompleted";
-export const FILTER_STATUS_CHANGED = "filters/statusFilterChanged";
