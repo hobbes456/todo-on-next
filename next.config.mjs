4b91fead16adf241244e78475d0a8b4d30e3e762
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-27 22:11:06 +0300

    change next.config: simplified import of styles #9

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+import path from "path";
+import fileUrl from "file-url";
+
+const __filename = fileUrl(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "src", "styles")],
+        prependData: `@use "styles/_helpers.scss" as *;`,
+    },
 };
 
 export default nextConfig;
