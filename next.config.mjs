8b812fb292df9c9979cf6c8d7a4442e50c183e2d
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-30 10:14:16 +0300

    change next-config: simplifying paths to styles #8

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+import path from "path";
+import fileUrl from "file-url";
+
+const __filename = fileUrl(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "src")],
+        prependData: `@use "src/styles/_helpers.scss" as *;`,
+    },
 };
 
 export default nextConfig;
